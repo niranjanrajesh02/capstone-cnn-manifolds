2348fe44deebff21ac55aa8a11982bb498f8a180
niranjanrajesh02 <niranjanrajesh02@gmail.com>
2023-11-21 22:38:57 +0530

    added model prediction code for sanity checks

--- cnns/predict_test.py
+++ cnns/predict_test.py
@@ -1,19 +1,36 @@
-from keras.applications.resnet50 import ResNet50, preprocess_input, decode_predictions
+import tensorflow as tf
+from keras.applications.xception import Xception, preprocess_input, decode_predictions
 from keras.preprocessing import image
 import numpy as np
+import argparse
+
+def predict_test(model_name, image_path):
+  if model_name == 'xception':
+    from keras.applications.xception import Xception, preprocess_input, decode_predictions
+    model = Xception(weights='imagenet')
+  elif model_name == 'vgg16':
+    from keras.applications.vgg16 import VGG16, preprocess_input, decode_predictions
+    model = VGG16(weights='imagenet')
+  elif model_name == 'resnet50':
+    from keras.applications.resnet50 import ResNet50, preprocess_input, decode_predictions
+    model = ResNet50(weights='imagenet')
 
 
-model = ResNet50(weights='imagenet')
+  img = image.load_img(image_path)
+  img_array = image.img_to_array(img)
+  img_array = np.expand_dims(img_array, axis=0)
+  img_array = preprocess_input(img_array)
 
-image_path = '../vid2img/frames/mug_test/frame_9.jpg'
+  predictions = model.predict(img_array)
 
-img = image.load_img(image_path, target_size=(224, 224))
-img_array = image.img_to_array(img)
-img_array = np.expand_dims(img_array, axis=0)
-img_array = preprocess_input(img_array)
+  decoded_predictions = decode_predictions(predictions, top=3)[0]
+  for i, (imagenet_id, label, score) in enumerate(decoded_predictions):
+      print(f"{i + 1}: {label} ({score:.2f})")
+  return decoded_predictions
 
-predictions = model.predict(img_array)
-
-decoded_predictions = decode_predictions(predictions, top=3)[0]
-for i, (imagenet_id, label, score) in enumerate(decoded_predictions):
-    print(f"{i + 1}: {label} ({score:.2f})")
+if __name__ == '__main__':
+  parser = argparse.ArgumentParser()
+  parser.add_argument('--model_name', type=str, default='xception', help='model name')
+  parser.add_argument('--image_path', type=str, default='../vid2img/frames/mug_test/frame_0.jpg', help='image path')
+  args = parser.parse_args()
+  predict_test(args.model_name, args.image_path)
--- vid2img/make_dataset.py
+++ vid2img/make_dataset.py
@@ -6,7 +6,7 @@ import shutil
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--video', type=str, default='mug_test', help='video file name')
-parser.add_argument('--size', type=str, default=224, help='size of frame')
+parser.add_argument('--size', type=str, default=299, help='size of frame')
 args = parser.parse_args()
 
 video_path = './videos/' + args.video + '.mp4'
@@ -27,7 +27,7 @@ success,image = vidcap.read()
 count = 0
 while success:
   # image = cv2.resize(image, (args.size, args.size), interpolation=cv2.INTER_AREA)
-  image = central_resize(image, (args.size, args.size))
+  image = central_resize(image, (int(args.size), int(args.size)))
   frame_path = f'{save_path}/frame_{count}.jpg'
   cv2.imwrite(frame_path, image)        
   print('Read + Saved a new frame: ', success)
