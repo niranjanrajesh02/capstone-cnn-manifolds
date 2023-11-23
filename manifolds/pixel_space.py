1a20f17e38083110ab79c6ff056c4d5c744824cc
niranjanrajesh02 <niranjanrajesh02@gmail.com>
2023-11-23 19:24:37 +0530

    frame extraction update

---
+++ manifolds/pixel_space.py
@@ -0,0 +1,9 @@
+import cv2
+import numpy as np
+import tensorflow as tf
+import keras
+from keras.utils import image_dataset_from_directory
+
+frames_path = "../vid2img/frames/"
+
+
---
+++ vid2img/get_frames.py
@@ -0,0 +1,42 @@
+import cv2
+import argparse
+import os
+from data_utils import center_crop, central_resize
+import shutil
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--video', type=str, default='mug',
+                    help='video file name')  # sunglass or mug
+parser.add_argument('--size', type=str, default=299, help='size of frame')
+args = parser.parse_args()
+
+video_path = './videos/' + args.video + '/'
+save_path = './frames/' + args.video + '/'
+
+
+if os.path.exists(save_path):
+    # delete the folder and its contents
+    shutil.rmtree(save_path)
+
+
+os.mkdir(save_path)
+
+videos = os.listdir(video_path)
+
+count = 0
+for video in videos:
+    video_path = video_path + video
+    vidcap = cv2.VideoCapture(video_path)
+    success, image = vidcap.read()
+
+    while success:
+        # image = cv2.resize(image, (args.size, args.size), interpolation=cv2.INTER_AREA)
+        image = central_resize(image, (int(args.size), int(args.size)))
+        frame_path = f'{save_path}/{args.video}_frame_{count}.jpg'
+        cv2.imwrite(frame_path, image)
+        print(f'Read + Saved a new frame {count}')
+        success, image = vidcap.read()
+        count += 1
+        # if count == 20:
+        #     break
+    print(f"Successfully read and saved {count} {args.video} frames")
